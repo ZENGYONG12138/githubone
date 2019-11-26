9deac30ddbb12ec261283edac6cf97a0886e6dbb
ZENG12138 <2969309639@qq.com>
2019-11-26 21:38:31 +0800

    项目2

---
+++ 极乐净土/极乐净土.cpp
@@ -0,0 +1,14 @@
+#include<graphics.h>
+#include<Windows.h>
+#include<mmsystem.h>
+#pragma comment(lib,"winmm.lib")
+
+int main(void)
+{
+	initgraph(800, 450);
+	loadimage(0, _T("jile.jpg"));
+	mciSendString(_T("play ¼«ÀÖ¾»ÍÁ.mp3 repeat"), 0, 0, 0);
+	system("pause");
+	closegraph();
+	return 0;
+}
