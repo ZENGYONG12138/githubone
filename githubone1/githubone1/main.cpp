ff1084afe2424666dde3ad16fe543eb189321c19
ZENG12138 <2969309639@qq.com>
2019-11-27 09:18:11 +0800

    测试项目2

--- githubone1/githubone1/main.cpp
+++ githubone1/githubone1/main.cpp
@@ -6,8 +6,33 @@
 using namespace std;
 
 int main(void) {
+	string name;
+	string pwd;
 
-	cout << "Hello Word !" << endl;
+	while (1) {
+		system("cls");
+
+		cout << "ÇëÊäÈëÕËºÅ£º";
+		cin >> name;
+
+		cout << "ÇëÊäÈëÃÜÂë£º";
+		cin >> pwd;
+
+		if (name == "54hk" && pwd == "123456") {
+			break;
+		} else {
+			system("pause");
+			cout << "ÓÃ»§»òÃÜÂë´íÎó£¡" << endl;
+		}
+	}
+	
+	system("cls");
+
+	cout << "1.ÍøÕ¾404¹¥»÷" << endl;
+	cout << "2.ÍøÕ¾´Û¸Ä¹¥»÷" << endl;
+	cout << "3.ÍøÕ¾¹¥»÷¼ÇÂ¼" << endl;
+	cout << "4.DNS¹¥»÷" << endl;
+	cout << "5.·þÎñÆ÷ÖØÆô¹¥»÷" << endl;
 
 	system("pause");
 	return 0;
